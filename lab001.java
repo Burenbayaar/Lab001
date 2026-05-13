7e738aff86eccad34e3ac6ec7db07e457c0fe74b
Burenbayr <91621822+Burenbayaar@users.noreply.github.com>
2026-05-13 13:39:47 +0800

    Add files via upload

---
+++ lab001.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public final class App {
+
+    /**
+     * Main method.
+     *
+     * @param args command line arguments
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * Private constructor.
+     */
+    private App() {
+    }
+}
